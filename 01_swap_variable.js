ead7c437d8d73a71c93976118201eb0801ba6411
Narkhedepratik6125 <pratiknarkhede6125@gmail.com>
2022-12-09 11:32:40 -0500

    swapping program
    swapping program

---
+++ 01_swap_variable.js
@@ -0,0 +1,11 @@
+var num1=100;
+var num2=200;
+var num3=300;
+console.log("befor swaping");
+console.log(num1,num2,num3);
+console.log("After swaping");
+var num4=num1
+num1=num2;
+num2=num3;
+num3=num4;
+console.log(num1,num2,num3);
